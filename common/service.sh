e8d0970f8d08aad88b5c846ff0859c70bcb0708b
Hitesh Thawani <hiteshthawani1995@gmail.com>
2019-05-03 16:19:07 +0530

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -5,5 +5,14 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
 # This script will be executed in late_start service mode
+
+#selinux stuff (havoc os)
+magiskpolicy --live "allow * init * *"
+magiskpolicy --live "allow init * * *"
+
+#restart camera
+stop qcamerasvr
+stop camera-provider-2-4
+start qcamerasvr
+start camera-provider-2-4
